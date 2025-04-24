01ae9d8e91d68ed0ee233a6b4490051702778396
Александр Мишаков <sasha.mishakov@gmail.com>
2025-04-24 19:48:22 +0300

    циклы

---
+++ 24-04-2025/example1.py
@@ -0,0 +1,28 @@
+"""
+Циклы - повторяющиеся действия в программе.
+
+Кумире:
+нц 4 раза
+    ...
+кц
+"""
+
+for i in range(4):
+    print(i)
+
+print("Конец цикла.")
+
+for i in range(10, 15):
+    print(i)
+
+print("Конец цикла.")
+
+for i in range(10, 20, 2):
+    print(i)
+
+print("Конец цикла.")
+
+for num in range(20, 15, -1):
+    print(num)
+
+print("Конец цикла.")
