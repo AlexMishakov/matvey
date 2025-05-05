ed5b7b38d940ba3e25ec791c9b880205e515f974
Александр Мишаков <sasha.mishakov@gmail.com>
2025-05-05 18:05:00 +0300

    шаг в for

---
+++ 05-05-2025/example1.py
@@ -0,0 +1,11 @@
+print("Только нечётные числа:")
+for i in range(1, 6, 2):
+    print(i)
+
+print("Только чётные числа:")
+for i in range(2, 7, 2):
+    print(i)
+
+print("Вывести все числа до 500 которые кратны 123:")
+for i in range(123, 500, 123):
+    print(i)
